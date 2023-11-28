ca2fefae861efdf253090948f018621c187b014f
Aryan Gupta <Aryangupta.x01@gmail.com>
2023-11-28 20:11:24 +0530

    cs

---
+++ Website/js/script.js
@@ -0,0 +1,10 @@
+// JavaScript to pause the animation on hover
+const slider = document.querySelector('.slider');
+
+slider.addEventListener('mouseover', () => {
+  slider.style.animationPlayState = 'paused';
+}); 
+
+slider.addEventListener('mouseout', () => {
+  slider.style.animationPlayState = 'running';
+});

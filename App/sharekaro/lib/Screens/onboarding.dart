8efc0ba744ee064cef404457d2c06a6fb15570ef
Gaurav Dubey <rmsd2804@gmail.com>
2023-11-28 22:51:47 +0530

    chg

---
+++ App/sharekaro/lib/Screens/onboarding.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+import 'package:floating_bubbles/floating_bubbles.dart';
+import 'package:flutter/material.dart';
+import 'package:sharekaro/Screens/homescreen.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+  static String id = 'LoginScreen';
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 100),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Positioned.fill(
+            child: FloatingBubbles.alwaysRepeating(
+              noOfBubbles: 50,
+              colorsOfBubbles: const [
+                Colors.white,
+                Colors.red,
+              ],
+              sizeFactor: 0.4,
+              opacity: 70,
+              speed: BubbleSpeed.slow,
+              paintingStyle: PaintingStyle.fill,
+              shape: BubbleShape.circle,
+            ),
+          ),
+          Positioned.fill(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'ShareKaro',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

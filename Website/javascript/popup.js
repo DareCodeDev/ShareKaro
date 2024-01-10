25f6e4081f649bb159f6579eb692c1f6e970087d
Aryan Gupta <Aryangupta.x01@gmail.com>
2024-01-10 12:18:22 +0530

    chaf

---
+++ Website/javascript/popup.js
@@ -0,0 +1,11 @@
+document.getElementById('show-login').addEventListener('click', function() {
+    // Toggle the visibility of the popup
+    var popup = document.querySelector('.popup');
+    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+  });
+
+  // Add event listener to close button
+  document.getElementById('closePopupBtn').addEventListener('click', function() {
+    // Close the popup when close button is clicked
+    document.querySelector('.popup').style.display = 'none';
+  });

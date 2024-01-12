471c679d503374647e8a398c662595c6c175af92
Aryan Gupta <Aryangupta.x01@gmail.com>
2024-01-12 14:46:10 +0530

    fakh

--- Website/javascript/popup.js
+++ Website/javascript/popup.js
@@ -4,6 +4,12 @@ document.getElementById('show-login').addEventListener('click', function() {
     popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
   });
 
+  document.getElementById('show_login').addEventListener('click', function() {
+    // Toggle the visibility of the popup
+    var popup = document.querySelector('.popup');
+    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+  });
+
   // Add event listener to close button
   document.getElementById('closePopupBtn').addEventListener('click', function() {
     // Close the popup when close button is clicked

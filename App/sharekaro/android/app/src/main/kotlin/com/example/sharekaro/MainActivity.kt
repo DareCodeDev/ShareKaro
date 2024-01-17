efd6bd75e26267e50dd7231f4994280f883b9cd9
Gaurav Dubey <rmsd2804@gmail.com>
2024-01-17 18:51:22 +0530

    chg

--- App/sharekaro/android/app/src/main/kotlin/com/example/sharekaro/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sharekaro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- App/sharekaro/lib/Constants/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color bg = Color(0xFFfcf4f1);
-Color primary = Color(0xffff9465);
-Color secondary = Color(0xffFF9800);
-Color text = Color.fromARGB(255, 0, 0, 0);
-Color text2 = Color.fromARGB(255, 255, 255, 255);
-Color button = Color(0xffeb6f31);
